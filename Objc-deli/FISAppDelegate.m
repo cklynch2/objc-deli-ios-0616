746bffad4462cd071fb4413b6c6eae533abfc3bd
cklynch2 <cklynch2@gmail.com>
2016-05-12 13:48:16 -0400

    Katzs deli is back in business

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,36 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine
+{
+    NSMutableString *announceDeliLine = [[NSMutableString alloc]init];
+    if ([deliLine count] > 0) {
+        announceDeliLine = [@"The line is:" mutableCopy];
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            NSInteger numberInLine = i + 1;
+            NSString *customerInLine = deliLine[i];
+            [announceDeliLine appendFormat:@"\n%li. %@", numberInLine,customerInLine];
+        }
+    }
+    else if ([deliLine count] == 0){
+        announceDeliLine = [@"The line is currently empty." mutableCopy];
+    }
+    return announceDeliLine;
+}
+
+
+-(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine
+{
+    [deliLine addObject:name];
+}
+
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine
+{
+    NSString *nextCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return nextCustomer;
+}
+
 
 @end
